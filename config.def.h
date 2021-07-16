09a299e3417f2521a70a348f4623e92ddf2808a5
Akash Sooraj <akashsooraj6493@gmail.com>
2021-07-16 19:28:48 +0530

    some helpful comments

--- config.def.h
+++ config.def.h
@@ -63,15 +63,15 @@ static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },	// spawn dmenu: mod + p
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },	// spawn dmenu: mod + d
 	{ MODKEY,             XK_Return, spawn,          {.v = termcmd } },	// spawn terminal: mod + enter
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },	// toggle bar: mod + b
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },	// climb up stack: mod + j
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },	// climb down stack: mod + k
+	{ MODKEY,                       XK_w,      incnmaster,     {.i = +1 } },	// increase number of windows in master: mod + w
+	{ MODKEY,                       XK_s,      incnmaster,     {.i = -1 } },	// decrease number of windows in master: mod + s
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },	// resize master: decrease master size: mod + h
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },	// resize master: increase master size: mod + l
 	{ MODKEY|ShiftMask,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },	// close window: mod + shift + q
@@ -86,8 +86,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_1,                      0)	// mod + 0 to move to tag 0
+	TAGKEYS(                        XK_2,                      1)	// and so on.. upto tag 9
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
@@ -95,7 +95,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },	// quit dwm: mod + shift + q
+	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },	// quit dwm: mod + shift + e
 };
 
 /* button definitions */
