1350a5f1d8cd12b9df7edb98640039022a3a8eb4
Akash Sooraj <akashsooraj6493@gmail.com>
2021-07-17 20:02:16 +0530

    change dwm colors

--- config.def.h
+++ config.def.h
@@ -8,15 +8,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray1[]       = "#000000"; // black; default: #222222
+static const char col_gray2[]       = "#004369"; // default: #444444
+static const char col_gray3[]       = "#fbb023"; // default:#bbbbbb;
+static const char col_gray4[]       = "#e5ddc8"; // default: #eeeeee
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray1, col_gray3,  col_cyan  },
 };
 
 /* tagging */
